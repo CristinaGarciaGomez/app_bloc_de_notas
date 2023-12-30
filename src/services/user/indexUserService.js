3e133f439d6166bbb58b93e0b828d79c87b4db3f
Cristina García Gómez <elccorreodecristina13@gmail.com>
2023-12-30 15:31:12 +0100

    postman ajustes

--- src/services/user/indexUserService.js
+++ src/services/user/indexUserService.js
@@ -3,15 +3,14 @@
 // Importamos todas las funciones  con el servicio de usuario
 import findUserByEmail from './userFunctionService/login/findUserByEmail.js';
 import comparePasswords from './userFunctionService/login/comparePasswords.js';
-
 import register from './userFunctionService/registerUserService.js';
 
-//exportamos funciones a controllers (registerController.js y loginController.js)
-export default {findUserByEmail, comparePasswords, register}
-
-
-
-
+//exportamos funciones a controllers (registerController.js y loginController.js
+export default {
+  findUserByEmail,
+  comparePasswords,
+  register
+}
 
 
 
